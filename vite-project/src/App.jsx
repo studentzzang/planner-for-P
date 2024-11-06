50747cfd95a1b1748401dd63cd845b76493d59f1
studentzzang <studentzzang@gmail.com>
2024-11-07 00:13:02 +0900

    asdfasdfasdfasdfasdfasdf

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+function App() {
+
+  return (
+    
+    <>
+    
+      <div className="bg-gradient-to-r from-green-100 to-cyan-100">
+        <div className="text-cyan-500">ㅎㅇ</div>
+      </div>
+    </>
+  )
+}
+
+export default App;

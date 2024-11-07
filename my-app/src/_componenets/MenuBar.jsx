31b5546b3378d54c410d3d3dc40edf73e24da1ca
studentzzang <studentzzang@gmail.com>
2024-11-08 00:46:32 +0900

    asdf

--- my-app/src/_componenets/MenuBar.jsx
+++ my-app/src/_componenets/MenuBar.jsx
@@ -1,11 +1,21 @@
-export default function MenuBar({currentTab, handleTab}){
-  return (
-    <div className="menu-bar flex justify-center items-center h-">
-      <div className="button-container w-3/4 bg-slate-500 flex items-center">
-        <button className="today">오늘의 미션</button>
-        <button className="today"></button>
-        <button className="today">오늘의 미션</button>
+  export default function MenuBar({currentTab, handleTab}){
+
+    const btnStyle = "block p-3 my-12 text-4xl text-teal-800 transition-all duration-300 ease-in-out";
+    const currentBtnStyle = "mx-5 text-4xl text-teal-500 font-semibold transition-all duration-300 ease-in-out";
+
+    return (
+      <div className="menu-bar flex justify-center items-center">
+        <div className="button-container w-5/6 my-1">
+
+          <button className={`${btnStyle} ${currentTab === 0 ? currentBtnStyle : ''}`} onClick={()=>handleTab(0)}>오늘의 미션</button>
+          <button className={`${btnStyle} ${currentTab === 1 ? currentBtnStyle : ''}`} onClick={()=>handleTab(1)}>주간 미션</button>
+          <button className={`${btnStyle} ${currentTab === 2 ? currentBtnStyle : ''}`} onClick={()=>handleTab(2)}>월간 미션</button>
+          <button className={`${btnStyle} ${currentTab === 3 ? currentBtnStyle : ''}`} onClick={()=>handleTab(3)}>연간 미션</button>
+        </div>
+
+        <div className="other-container">
+
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }

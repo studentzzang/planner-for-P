4af3fb96abf5db995f1f0c7a44b2164ecdd8688b
studentzzang <studentzzang@gmail.com>
2024-12-13 10:33:23 +0900

    d

--- my-app/src/firebase/Backend.js
+++ my-app/src/firebase/Backend.js
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "planner-for-p.firebaseapp.com",
